89ad2c5ed1dd0cc1057e96c861a5f50b9655d1df
Cristian Morales <cr15moresp@gmail.com>
2021-03-06 16:05:54 -0600

    Diferentes maneras de imprimir variables

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
 
 
-console.log( console.log('Hola mundo') );
+// console.log( console.log('Hola mundo') );
+
+// var   b = 10;  // Declaración de variables en el anterior JS
+// const c = 10;  // Mala practica de declaracion de variables
+
+let   a = 10,  // Como se declaran en la actualidad
+      b = 20,  // Como se declaran en la actualidad
+      c = 30,  // Como se declaran en la actualidad
+      d = 40,  // Como se declaran en la actualidad
+      e = 'Hola ',  // Como se declaran en la actualidad
+      f = 'Spiderman',  // Como se declaran en la actualidad
+      x = a + b;  // Como se declaran en la actualidad
+
+const saludo = e + f;
+
+console.log('a', a); // estan los metodos log, error, info, warn
+console.log('b', b);
+console.log('c', c);
+console.log('d', d);
+
+
+console.log('%c Mis variables', 'color:blue; font-weight:bold'); // Aplicar estilo de CSS
+console.log( { a } );
+console.log( { b } );
+console.log( { c } );
+console.log( { d } );
+
+console.table( {a,b,c,e,f} );
 
