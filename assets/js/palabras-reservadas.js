e60f38c0641d9dd76e06e640c1b5532384d1c8f5
Cristian Morales <cr15moresp@gmail.com>
2021-03-07 14:23:25 -0600

    Se trato el nombre de variables y un pdf con las palabras reservadas de JS

---
+++ assets/js/palabras-reservadas.js
@@ -0,0 +1,6 @@
+
+
+// let const = 5; // Marca error por se una palabra reservada
+
+// No usar nombres raros de variables, hacerlo similar a como ya se ha hecho en otros lenguajes de programacion
+// tampoco usar nombres de las palabras que ya son propias del lenguaje
