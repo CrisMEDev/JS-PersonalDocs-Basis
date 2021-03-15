c0a637a5354ccd7dbfc70b4e84fc8bf86d450c82
Cristian Morales <cr15moresp@gmail.com>
2021-03-15 16:39:31 -0600

    Se analizó la estructura switch

---
+++ assets/js/switch.js
@@ -0,0 +1,38 @@
+
+const dia = 0; // 0: Domingo
+
+switch( dia ){ // Por defecto hace un triple ===
+    case 0:
+        console.log('Lunes');
+    break;
+    
+    case 1:
+        console.log('Lunes');
+    break;
+
+    case 2:
+        console.log('Lunes');
+    break;
+
+    case 3:
+        console.log('Lunes');
+    break;
+
+    case 4:
+        console.log('Lunes');
+    break;
+
+    case 5:
+        console.log('Lunes');
+    break;
+
+    case 6:
+        console.log('Lunes');
+    break;
+
+    default:
+        console.log('Valor no permitido');
+}
+
+
+
