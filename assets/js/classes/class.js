99398383955ba2a76ee750e592dc03781285ff86
Cristian Morales <cr15moresp@gmail.com>
2021-04-11 14:22:07 -0500

    Se creó una clase básica en JS con su constructor

---
+++ assets/js/classes/class.js
@@ -0,0 +1,27 @@
+
+class Persona { // Por defecto todas las clases en JS usan el 'use strict'
+
+    // La sintaxis para inicializar propiedades de clase es la siguiente; no es como inicializar variables
+    // ya que si se intenta hacer todas las variables en una linea separadas por coma daria error
+    nombre = '';
+    codigo = '';
+    frase = '';
+
+    // Constructor
+    constructor(nombre = 'Sin nombre', codigo = 'Sin código', frase = 'Sin frase') {
+        // Si las variables no están inicializadas  en el argumento de funcion; entonces tendrán un undefined
+        if ( !nombre ) throw Error('Por favor coloque un nombre');
+
+        // console.log('Hola!');
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.frase = frase;
+    }
+
+}
+
+const spiderman = new Persona('Peter Parker', 'Spiderman', 'Soy tu amigable vecino spiderman');
+const ironman = new Persona('Tony Stark', 'Ironman', 'Yo soy ironman');
+console.log( spiderman );
+console.log( ironman );
+
