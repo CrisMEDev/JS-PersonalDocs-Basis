3accff1a32115a0628bd4cc7b27f23514e6f6b94
Cristian Morales <cr15moresp@gmail.com>
2021-04-11 15:10:04 -0500

    Se implementó el get y set para la comida de Persona; se analizaron los casos de mala práctica

--- assets/js/classes/class.js
+++ assets/js/classes/class.js
@@ -4,8 +4,9 @@ class Persona { // Por defecto todas las clases en JS usan el 'use strict'
     // La sintaxis para inicializar propiedades de clase es la siguiente; no es como inicializar variables
     // ya que si se intenta hacer todas las variables en una linea separadas por coma daria error
     nombre = '';
-    codigo = '';
-    frase = '';
+    codigo = '';     // Es opcional deljar las propiedades aqui, ya que si se comentan, como siguen en el
+    frase  = '';     // constructor funcionarán igual
+    comida = '';
 
     // Constructor
     constructor(nombre = 'Sin nombre', codigo = 'Sin código', frase = 'Sin frase') {
@@ -16,6 +17,14 @@ class Persona { // Por defecto todas las clases en JS usan el 'use strict'
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
+    }
+
+    set setComidaFavorita( comida ){    // El nombre del metodo set no debe ser el mismo que la variable set
+        this.comida = comida;           // ya que al asignarlo provocaria un bucle infinito
+    }
+
+    get getComidafavorita(){
+        return this.comida.toUpperCase();
     }
 
     quienSoy() {
@@ -31,9 +40,17 @@ class Persona { // Por defecto todas las clases en JS usan el 'use strict'
 
 const spiderman = new Persona('Peter Parker', 'Spiderman', 'Soy tu amigable vecino spiderman');
 const ironman = new Persona('Tony Stark', 'Ironman', 'Yo soy ironman');
-console.log( spiderman );
 console.log( ironman );
 
 spiderman.quienSoy();
 // ironman.quienSoy();
 ironman.miFrase();
+
+spiderman.setComidaFavorita = 'El pie de cereza de la tía May';
+// spiderman.nemesis = 'Duente Verde'; // Evitar usar estas maneras de crear propiedades en clases
+                                       // en este caso solo spiderman será el objeto que tenga esa propiedad
+
+// spiderman.comida = 'Duende Verde';  // Evitar hacer esto aunque se pueda; aun no está implememntado de manera oficial
+                                    // el uso de propiedades privadas de JS para evitar esto; sin embargo chrome lo admite
+console.log( spiderman );
+console.log( spiderman.getComidafavorita );
